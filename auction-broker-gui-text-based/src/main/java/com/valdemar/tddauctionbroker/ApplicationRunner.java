46de75a76ca43b2e4cd9aeae60efb1ae93e3c19c
valdemarpereira <valdemar.pereira@gmail.com>
2014-02-08 18:40:37 +0000

    inttest and FakeServer

---
+++ auction-broker-gui-text-based/src/main/java/com/valdemar/tddauctionbroker/ApplicationRunner.java
@@ -0,0 +1,38 @@
+package com.valdemar.tddauctionbroker;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: valdemar
+ * Date: 08-02-2014
+ * Time: 17:46
+ * To change this template use File | Settings | File Templates.
+ */
+public class ApplicationRunner {
+    public static final String SNIPER_ID = "sniper";
+    public static final String SNIPER_PASSWORD = "sniper";
+    private AuctionSniperDriver driver;
+
+    public void startBiddingIn(final FakeAuctionServer auction) {
+        Thread thread = new Thread("Test Application") {
+            @Override public void run() {
+                try {
+                    Main.main(XMPP_HOSTNAME, SNIPER_ID, SNIPER_PASSWORD, auction.getItemId());
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        thread.setDaemon(true);
+        thread.start();
+        driver = new AuctionSniperDriver(1000);
+        driver.showsSniperStatus(Main.STATUS_JOINING);
+    }
+    public void showsSniperHasLostAuction() {
+        driver.showsSniperStatus(Main.STATUS_LOST);
+    }
+    public void stop() {
+        if (driver != null) {
+            driver.dispose();
+        }
+    }
+}
---
+++ auction-broker-inttest/src/test/java/com/valdemar/tddauctionbroker/AuctionSniperEndToEndTest.java
@@ -0,0 +1,31 @@
+package com.valdemar.tddauctionbroker;
+
+import org.junit.After;
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AuctionSniperEndToEndTest {
+
+private final FakeAuctionServer auction = new FakeAuctionServer("item-54321");
+private final ApplicationRunner application = new ApplicationRunner();
+
+        @Test
+        public void sniperJoinsAuctionUntilAuctionCloses() throws Exception {
+            auction.startSellingItem();                 // Step 1
+            application.startBiddingIn(auction);        // Step 2
+            auction.assertThatHasReceivedJoinRequestFromSniper(); // Step 3
+            auction.announceClosed();                   // Step 4
+            application.showsSniperHasLostAuction();    // Step 5
+        }
+
+        // Additional cleanup
+        @After
+        public void stopAuction() {
+            auction.stop();
+        }
+        @After public void stopApplication() {
+            application.stop();
+        }
+}
